f024d1f5347ce4a5bc3b2ac4d2d8db816df47cb3
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2022-02-28 18:14:41 -0500

    add debouncing to save logic:

--- packages/cli/dist/notebook.js
+++ packages/cli/dist/notebook.js
@@ -1,1 +1,1 @@
-[{"content":"sdasdasddasdaasdadadasdadssdadsdadssad","type":"code","id":"f80ag"},{"content":"asdadasdadsad","type":"text","id":"up8g1"},{"content":"","type":"code","id":"2jill"},{"content":"","type":"text","id":"ozblm"}]
+[{"content":"sdasdasddasdaasdadadasdadssdadsdadssadasdadsadasdadasdasdsdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdsadasdasdasdasdasdasdasdsasd","type":"code","id":"f80ag"},{"content":"asdadasdadsadasdadsdasdasddasdas","type":"text","id":"up8g1"},{"content":"asdasdadasdasd","type":"code","id":"2jill"},{"content":"","type":"text","id":"ozblm"}]
