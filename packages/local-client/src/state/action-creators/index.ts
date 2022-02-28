1a48a241beb513155b01b34949ba7637168b2f3b
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2022-02-28 17:27:11 -0500

    set up fetch cells error action

--- packages/local-client/src/state/action-creators/index.ts
+++ packages/local-client/src/state/action-creators/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { ActionType } from '../action-types';
 import { Cell, CellTypes } from '../cell';
 import bundle from '../../bundler';
+import { RootState } from '../reducers';
 import {
   Action,
   UpdateCellAction,
@@ -73,7 +74,7 @@ export const createBundle = (cellId: string, input: string) => {
   };
 };
 
-export const fetchCell = () => {
+export const fetchCells = () => {
   return async (dispatch: Dispatch<Action>) => {
     dispatch({ type: ActionType.FETCH_CELLS });
 
@@ -86,6 +87,24 @@ export const fetchCell = () => {
     } catch (err: any) {
       dispatch({
         type: ActionType.FETCH_CELLS_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+
+    const cells = order.map((id) => data[id]);
+    try {
+      await axios.post('/cells', { cells });
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.SAVE_CELLS_ERROR,
         payload: err.message,
       });
     }
