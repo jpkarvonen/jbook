5a8c2be3cd825003a287f44f0ae86136d1fcc5b0
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2022-03-01 12:09:29 -0500

    untrack node_modules, dist and build files

--- packages/cli/dist/notebook.js
+++
@@ -1,1 +0,0 @@
-[{"content":"sdasdasddasdaasdadadasdadssdadsdadssadasdadsadasdadasdasdsdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdsadasdasdasdasdasdasdasdsasd","type":"code","id":"f80ag"},{"content":"asdadasdadsadasdadsdasdasddasdas","type":"text","id":"up8g1"},{"content":"asdasdadasdasd","type":"code","id":"2jill"},{"content":"","type":"text","id":"ozblm"}]
